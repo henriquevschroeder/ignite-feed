529ce4910604331da2675382b3f76bb4002701fa
henriquevschroeder <henriquevschdev@gmail.com>
2024-06-16 21:49:19 -0300

    feat: add Comment component

---
+++ src/components/Comment.tsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/diego3g.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Henrique Schroeder</strong>
+              <time title="June 6, 2024 at 9pm" dateTime="2024-06-16 21:00:00">Around 1h ago</time>
+            </div>
+
+            <button title="Delete comment">
+              <Trash size={24} />
+            </button>        
+          </header>
+          
+          <p>Really cool! 😄</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Cheer <span>21</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
