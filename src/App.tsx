6d34f867eeb9f66fb75bc29bd5b7355c77e1531f
henriquevschroeder <henriquevschdev@gmail.com>
2024-06-16 21:30:37 -0300

    feat: add Post component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { Header } from './components/Header'
+import styles from './App.module.css'
 import './global.css'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
-    <>
+    <div>
       <Header />
-      <h1>Hello, World!</h1>
-    </>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Henrique Schroeder"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit." />
+          <Post
+            author="Henrique Schroeder"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit." />
+        </main>
+      </div>
+    </div>
   )
 }
+
---
+++ src/components/Post.tsx
@@ -0,0 +1,42 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/henriquevschroeder.png" />
+          <div className={styles.authorInfo}>
+            <strong>Henrique Schroeder</strong>
+            <span>Full Stack Developer</span>
+          </div>
+        </div>
+
+        <time title="June 6, 2024 at 9pm" dateTime="2024-06-16 21:00:00">Published 1h ago</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>What's up people! 👋</p>
+        <p>I just uploaded another project to my portfolio. It's a project I built during a Rocketseat course. The name of the project is Gympass API🚀</p>
+        <p>
+          <a href="https://github.com/henriquevschroeder/gympass-api">  https://github.com/henriquevschroeder/gympass-api</a>
+        </p>
+        <p>
+          <a href="#">#newproject</a>{' '}
+          <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Leave your feedback</strong>
+
+        <textarea placeholder="Leave a oomment" />
+
+        <footer>
+          <button type="submit">Publish</button>
+        </footer>
+      </form>
+    </article>
+  )
+}
